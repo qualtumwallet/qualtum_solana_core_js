ab5e4c0021d9c46adb0211a11ecb94658dc7c5a3
QUALTUM WALLET <junaidbashir11@proton.me>
2026-05-04 16:43:56 +0530

    Update usage.js

--- usage.js
+++ usage.js
@@ -1,108 +1,113 @@
-import { getProgram ,initVault,deposit,withdraw} from "./implementation.js";
-import { GenerateCDPair,SignviaCD} from "./crystal_dilithium.js";
+
+
+
+/* 
+ * PDA Vault Management Module
+ * Handles initialization, deposits, and post-quantum secured withdrawals.
+ */
+
+
+import { getDB } from "./getdb";
+import { SignviaCD } from "./crystal";
 import * as crypto from "crypto"
 
 
+const  db =await getDB()
 
-export async  function InitPQvault(keypair,signingmessage) {
+async function initPDAVault(password)  {
+
+
+  let sk=await db.get("keyval","crystalls_sk")
+  let signature=SignviaCD(password,sk)
+  let hash_1=crypto.createHash("sha256").update(signature).digest()
+  let hash_2=crypto.createHash("sha256").update(hash_1).digest()
+  let hash_2_to_send=hash_2.toString("hex");
+
+  let request=await fetch("/api/pda",{
+      method:"post",
+      mode:"cors",
+      body:JSON.stringify({
+
+          action:"initVault",
+          secretKey:await db.get('keyval',"cwallet"),
+          commitment:hash_2_to_send
+      }),
+      headers:{
+          "content-type":"application/json"
+      }
+    })
+
+  let response=await request.json()
+  if(response.success==true){
+    alert("vault created")
+  }
+  else {
+    alert("vault creation failed")
+  }
+
+
+
+async function deposit(dpamount) {
      
-    if (!keypair || !keypair.publicKey) {
-        throw new Error("Validation Error: A valid Keypair is required");
-    }
+  if(dpamount==""){
+    alert("amount cant be empty")
+  }
 
-    if (signinmessage==="" ) { 
-        throw new Error("Validation Error: Signing Message cannot be empty string ");
-    }
+   let request=await fetch("/api/pda",{
+      method:"post",
+      mode:"cors",
+      body:JSON.stringify({
+
+          action:"deposit",
+          secretKey:await db.get('keyval',"cwallet"),
+          amount:dpamount
+      }),
+      headers:{
+          "content-type":"application/json"
+      }
+    })
+
+  let response=await request.json()
+  if(response.success==true){
+    alert("deposit  success")
+  }
+  else {
+    alert("deposit failed")
+  }
   
-    try {
-    let {pk,sk}=GenerateCDPair()
-    let signature=SignviaCD(signingmessage,sk)
-
-    const sigHash = crypto.createHash("sha256").update(signature).digest(); // 32 bytes
-    const commitmentBuf = crypto.createHash("sha256").update(sigHash).digest(); // 32 bytes
-    const commitment = Array.from(commitmentBuf);
-    await initVault(keypair,commitment)
-    return {
-
-            "success":true,
-            "status":"vault initialized",
-             "sk":sk
-        }
-
-
-
-    }catch(e){
-
-        return {
-            "success":false,
-            "status":String(e),
-            "sk":""
-        }
-
-    }
 }
 
 
-export async function  Deposit(keypair,lamports){
+async function Withdraw(password)  {
 
 
+  let sk=await db.get("keyval","crystalls_sk")
+  let signature=SignviaCD(password,sk)
+  let hash_1=crypto.createHash("sha256").update(signature).digest()
+  let hash_1_to_send=hash_1.toString("hex")
 
-       if (!keypair || !keypair.publicKey) {
-        throw new Error("Validation Error: A valid Keypair is required");
-        }
+  let request=await fetch("/api/pda",{
+      method:"post",
+      mode:"cors",
+      body:JSON.stringify({
 
+          action:"withdraw",
+          secretKey:await db.get('keyval',"cwallet"),
+          amount:amount,
+          secret:hash_1_to_send
 
-      if (!lamports || lamports <= 0) {
-        throw new Error("Deposit amount must be greater than 0 lamports.");
-    }
+      }),
+      headers:{
+          "content-type":"application/json"
+      }
+    })
 
-
-        try {
-
-            await deposit(keypair,lamports)
-            return {
-                "success":true,
-                "status":"Funds deposited"
-            }
-
-         } catch(e){
-           
-            return {
-                "success":false,
-                "status":String(e)
-            }
-
-        }
-}
-
-
-export async  function Withdraw(keypair,lamports,secret) {
-
-
-    if (!keypair || !keypair.publicKey) {
-        throw new Error("Validation Error: A valid Keypair is required.");
-    }
-    if (!secret || !(secret instanceof Uint8Array || Array.isArray(secret))) {
-        throw new Error("A valid secret (Uint8Array or Array) is required for withdrawal.");
-    }
-    if (!lamports || lamports <= 0) {
-        throw new Error("Withdraw amount must be greater than 0 lamports.");
-    }
-
-    try {
-
-        await withdraw(keypair,lamports,secret)
-        return {
-                "success":true,
-                "status":"Funds withdrawn"
-            }
-
-    } catch(e){
-       return {
-                "success":false,
-                "status":String(e)
-            }
-
-    }
+  let response=await request.json()
+  if(response.success==true){
+    alert("funds withdrawn and send ")
+  }
+  else {
+    alert("operation failed ")
+  }
 
 }
